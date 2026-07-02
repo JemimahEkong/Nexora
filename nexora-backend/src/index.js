75de3dcb37b30bc583310d721c82af62f916d381
Gloria Enyi <nkemaconam@gmail.com>
2026-07-02 08:37:35 +0100

    update index.js to accommodate frontend deployment

--- nexora-backend/src/index.js
+++ nexora-backend/src/index.js
@@ -30,6 +30,7 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
   'http://localhost:3001',
+  'https://nexora-recon.vercel.app',
 ].filter(Boolean);
 
 app.use(cors({
